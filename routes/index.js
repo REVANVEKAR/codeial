a96f54ed069d3161b5e901d4dac299468269baf1
Revanth N Anvekar <revanthanvekar11@gmail.com>
2023-10-29 23:56:48 +0530

    add users/profile route

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 
+const homeController = require('../controllers/home_controller');
+
 const router = express.Router();
 
-console.log('router loaded')
+// console.log('router loaded')
 
+router.get('/',homeController.home);
+router.use('/users', require('./users'))
+
+//for any further routes, access from here
+//router.use('/routeName',require('./routerfile));
 
 module.exports = router;
