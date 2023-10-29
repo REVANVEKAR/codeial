3645077e73a494c394f5c49a84bb9feb8af160c5
Revanth N Anvekar <revanthanvekar11@gmail.com>
2023-10-30 00:23:51 +0530

    view engine setup

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const port = 8000;
 //use express router 
 app.use('/',require('./routes/index'))
 
+app.set('view engine','ejs');
+app.set('views','./views')
 
 app.listen(8000,function(error){
     if (error) {
